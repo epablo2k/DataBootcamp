fc853ab16986c2b98427b0b476a510109f4bd023
epablo2k <52604806+epablo2k@users.noreply.github.com>
2019-08-23 10:56:00 -0500

    Add files via upload

---
+++ python-challenge/PyParagraph/main.py
@@ -0,0 +1,49 @@
+﻿import re
+
+files = {
+					 '1' : 'p1.txt',
+           '2' : 'p2.txt',
+           '3' : 'p3.txt',
+}
+
+print ("Enter file number (1,2,3):")
+filenumber = input()
+
+while (not(filenumber in files.keys())):
+	print ("Enter file number (1,2,3):")
+	filenumber = input()
+
+
+
+words=re.findall("\w+", open(files[filenumber]).read().lower())
+sentences=re.findall(r".*?\.", open(files[filenumber]).read().lower())
+
+#print ("%s" % len(words))
+#print ("%s" % words)
+#print ("%s" % sentences)
+
+
+wordcount = 0
+totalletters = 0
+
+for word in words:
+	totalletters = totalletters + len(word)
+	wordcount = wordcount + 1
+	
+averagewordlen = 	totalletters / wordcount
+
+sentencecount = 0
+totalwords = 0
+
+for sentence in sentences:
+	words2=re.findall("\w+", sentence.lower())
+	totalwords = totalwords + (len(words2)-1)
+	sentencecount = sentencecount + 1
+	
+averagesentence = 	totalwords / sentencecount
+
+
+print ("Total words found: %s" % (len(words)-1))
+print ("Average letters per word: %s" % round(averagewordlen,2))
+print ("Total sentences found: %s" % (len(sentences)))
+print ("Average word per sentence: %s" % round(averagesentence,2))
