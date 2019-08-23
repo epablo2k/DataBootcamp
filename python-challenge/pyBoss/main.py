72142d78edb68958a52042d3d7299db6957fcf4b
epablo2k <52604806+epablo2k@users.noreply.github.com>
2019-08-23 10:49:22 -0500

    Add files via upload

---
+++ python-challenge/pyBoss/main.py
@@ -0,0 +1,98 @@
+﻿def left(s, amount):
+    return s[:amount]
+
+def right(s, amount):
+    return s[-amount:]
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Palau': 'PW',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
+
+f=open("employee_data.csv", "r")
+fo=open("employee_data_out.csv","w+")
+fo.write("Emp ID,FirstName,LastName,DOB,SSN,State\n")
+num = 1
+
+f1 = f.readlines()
+
+for x in f1:
+
+	if num > 1:
+		print(x)
+		
+		lineid,fullname,dob,ssn,state = x.split(",")
+		lastname,firstname = fullname.split(" ")
+		year,month,day = dob.split("-")
+		lastssn = right(ssn,4)
+		stateab = us_state_abbrev[state.replace("\n","")]
+		#state.replace('\r', '')
+		
+		fo.write("%s,%s,%s,%s/%s/%s,***-**-%s,%s\n" % (lineid,lastname,firstname,month,day,year,lastssn,stateab))
+		
+	num = num + 1
+	
+print(f"total lines procesed {num}\n")
+print(f"Output File successfully written employee_data_out.csv")
+f.close() 
+fo.close() 
+
+
+
